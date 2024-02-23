b2cc634b247c07f77e9b9a22fda5306ae94815d6
Yusuf Demirkılıç <yusuf.demirkilic1@gmail.com>
2024-02-23 21:26:48 +0300

    Dimond program

---
+++ src/Star.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Star {
+    public static void main(String[] args) {
+        int n, a;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Pleas in a number");
+        n = scanner.nextInt();
+        a = n - 1;
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j < (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int m = 1; m < n; m++) {
+            for (int v = 0; v < m; v++) {
+                System.out.print(" ");
+            }
+            for (int f = 0; f < 2 * a - 1; f++) {
+                System.out.print("*");
+            }
+            System.out.println();
+            a--;
+        }
+    }
+}
