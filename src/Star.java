a93845d29de33ee844d3e0e7a0f67154fbb3dd74
Yusuf Demirkılıç <yusuf.demirkilic1@gmail.com>
2024-02-24 00:35:29 +0300

    açıklama eklendi

--- src/Star.java
+++ src/Star.java
@@ -7,15 +7,17 @@ public class Star {
         System.out.println("Pleas in a number");
         n = scanner.nextInt();
         a = n - 1;
+        // tek sayı ile boşluk yapıldı
         for (int i = 0; i <= n; i++) {
             for (int j = 0; j < (n - i); j++) {
                 System.out.print(" ");
-            }
+            }//yıldız yapıldı
             for (int k = 0; k < (2 * i - 1); k++) {
                 System.out.print("*");
             }
             System.out.println();
         }
+        //üçgenin altı için 1 adm ileriden başlandı
         for (int m = 1; m < n; m++) {
             for (int v = 0; v < m; v++) {
                 System.out.print(" ");
