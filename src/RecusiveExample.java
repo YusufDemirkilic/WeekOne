2f5c6ec4cd6ee21abf902e4d961e6c475f7b6f2c
Yusuf Demirkılıç <yusuf.demirkilic1@gmail.com>
2024-02-27 00:05:24 +0300

    example upload description

--- src/RecusiveExample.java
+++ src/RecusiveExample.java
@@ -2,13 +2,15 @@ import java.util.Scanner;
 
 public class RecusiveExample {
     static void f(int n) {
-
+        //kullanıcdan alınan değeri 0 değerine gelne kadar yazdırır
         if (n <= 0) {
             System.out.print(n + " ");
+            //- değeri yazdıktan sonra proamı 12. satıra geri götürür
             return ;
         }
         System.out.print(n+" ");
         f(n-5);
+        // return ile geriye değer döndürür
         System.out.print(n+" ");
     }
     public static void main(String[] args) {
